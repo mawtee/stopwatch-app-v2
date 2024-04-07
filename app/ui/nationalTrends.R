e358de73f3866908d4724aeae22e5b92304c59d4
benjcowley <benjaminjamescowley@gmail.com>
2024-04-07 22:00:02 +0100

    sorted

--- app/ui/nationalTrends.R
+++ app/ui/nationalTrends.R
@@ -167,7 +167,7 @@ nationalTrends <- (
                 choices=levels(df_pfa$year), selected=levels(df_pfa$year)[1],
                 animationOptions(interval=3500, loop=T)
               ),
-              style="position: absolute; top: 40%; right: 12%; z-index:1"
+              style="position: absolute; top: 8%; right: 60%; z-index:1"
               )
             ),
             #div(wellPanel(h1("panel")), style="width=100%", height="10%"),
